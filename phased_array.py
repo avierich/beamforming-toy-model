40aebb9104d62af8c1ce247a68c24fb3f1f41497
Alex Vierich <avierich@gmail.com>
2025-03-31 21:40:56 -0700

    Changing formatting

--- phased_array.py
+++ phased_array.py
@@ -3,23 +3,22 @@ import numpy as np
 import matplotlib.animation as animation
 
 
-frames = 400  # Number of frames in the animation
-RESOLUTION = 512
-grid_size = (RESOLUTION, RESOLUTION)  # Size of heatmap grid
+
+frames = 600  # Number of frames in the animation
+RESOLUTION = 1024
+RESOLUTION_V = int(RESOLUTION*5/8)
+grid_size = (RESOLUTION, RESOLUTION_V)  # Size of heatmap grid
 
 
-
-
-NOISE = 0.000001
+NOISE = 0.001
 
 NUM_TRANSMITTERS = 16
 
 THETA = np.pi*(180+20)/180.0
 THETA_B = np.pi*(180-10)/180.0
 
-TX_OFFSET = 0
+TX_OFFSET = 10
 
-# transmitters = [(x,TX_OFFSET) for x in range(int(grid_size[0]/4)+int(grid_size[0]/32), int(3*grid_size[0]/4), int(grid_size[0]/(4*NUM_TRANSMITTERS)))]
 
 OMEGA = 0.25
 
@@ -31,6 +30,7 @@ transmitters = [(n*tx_spacing + RESOLUTION/2 - NUM_TRANSMITTERS*tx_spacing/2+tx_
 
 
 # Precompute transmitter to cell distances
+print("===== Begin distance Precomputing =====")
 tx_distances=[]
 
 for tx in transmitters:
@@ -39,7 +39,8 @@ for tx in transmitters:
         r = np.sqrt(np.power(tx[0]-index[0], 2) + np.power(tx[1]-index[1],2))
         distances[index] = r
     tx_distances.append(distances)
-
+    print("One TX complete")
+print("===== Distance Computation Complete =====")
 
 
 def tx(r, beta_x, t):
@@ -60,11 +61,7 @@ def rx_probe(rx_pos, tx_theta):
     return power/N_SAMPLES
         
 
-
-
 field = np.empty(grid_size)
-
-
 
 ## New generate data
 def generate_data(theta, t):
@@ -78,33 +75,45 @@ def generate_tx_points():
     points_x = []
     points_y = []
     for transmitter in transmitters:
-        points_x.append(transmitter[0])
-        points_y.append(transmitter[1])
+        points_x.append(RESOLUTION_V-transmitter[1])
+        points_y.append(transmitter[0])
     return points_x, points_y
 
 # Initialize figure
-fig, (ax, ax2) = plt.subplots(1,2,figsize=(14, 6))
+fig, (ax, ax2) = plt.subplots(2,1,figsize=(6, 8),gridspec_kw={'height_ratios':[2,1]})
 heatmap_data = generate_data(THETA,0)
-heatmap = ax.imshow(heatmap_data, vmin=-1.0, vmax=1.0, cmap="coolwarm", interpolation='sinc', animated=True)
+heatmap = ax.imshow(np.flip(heatmap_data.T,0), vmin=-1.0, vmax=1.0, cmap="coolwarm", interpolation='sinc', animated=True)
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+ax.set_title("Beamforming with 16 Tx Antenna Array, 2 Beams, 2 UEs")
 tx_points = generate_tx_points()
 scatter = ax.scatter(tx_points[1], tx_points[0], marker = 'v',edgecolors='black')
-rx_scatter = ax.scatter([100,100], [23,23], marker = 'v',edgecolors='black', color = 'orange')
+rx_scatter = ax.scatter([100,100], [23,23], marker = 'v',edgecolors='black', color = 'orange', s=50)
 
 annotationA = ax.annotate(
-    'Rx A', xy=(1,0), xytext=(-1,0),
+    'UE A', xy=(1,0), xytext=(-1,0),
+    bbox=dict(boxstyle="round",
+                   ec=(1., 0.5, 0.2),
+                   fc=(1., 0.8, 0.5),
+                   )
 )
 annotationB = ax.annotate(
-    'Rx B', xy=(1,0), xytext=(-1,0),
+    'UE B', xy=(1,0), xytext=(-1,0),
+    bbox=dict(boxstyle="round",
+                   ec=(1., 0.5, 0.2),
+                   fc=(1., 0.8, 0.5),
+                   )
 )
 
 START_ANGLE = np.pi*(-60)/180.0 
 ANGLE_DELTA = np.pi*(120)/180.0
 
 sum_rate_plot, = ax2.plot(np.linspace(START_ANGLE, START_ANGLE+ANGLE_DELTA, frames), np.zeros(frames))
-ax2.set_ylim([0,30])
+sum_scatter = ax2.scatter(0,0)
+ax2.set_ylim([0,20])
 ax2.set_xlim([0,120])
-ax2.set_title("Sum Rate")
-ax2.set_ylabel("Rate")
+ax2.set_title("Sum Rate vs Angular Separation (SDMA)")
+ax2.set_ylabel("Sum Rate")
 ax2.set_xlabel("$\Delta\\theta$ (Degrees)")
 
 fig.tight_layout()
@@ -114,30 +123,25 @@ angles = []
 sum_rates = []
 # Update function for animation
 def update(frame):
-    # heatmap.set_array(generate_data(THETA + 2*np.pi*frame/frames, frame))  # Update heatmap data]
 
     print("Frame: "+str(frame))
 
     theta_a = np.pi*(60)/180.0
-    theta_b = START_ANGLE + ANGLE_DELTA*frame/frames
+
+    if(frame<frames/2):
+        theta_b = START_ANGLE + 2*ANGLE_DELTA*frame/frames
+    else:
+        theta_b = START_ANGLE+ANGLE_DELTA - 2*ANGLE_DELTA*(frame-frames/2)/frames
 
     # rx a position:
     rx_pos_a = (int(grid_size[0]/2-(RESOLUTION/2)*np.sin(theta_a)), int((RESOLUTION/2)*np.cos(theta_a))+TX_OFFSET)
     rx_pos_b = (int(grid_size[0]/2-(RESOLUTION/2)*np.sin(theta_b)), int((RESOLUTION/2)*np.cos(theta_b))+TX_OFFSET)
     print(rx_pos_a)
 
-    heatmap.set_array(0.5*generate_data(theta_a, frame)+0.5*generate_data(theta_b, frame))  # Update heatmap data]
-
-
-
-    # snir = rx_probe((23,100),THETA)/rx_probe((23,100),THETA_B)
-    # print(np.log2(1+SNIR))
-
+    heatmap.set_array(np.flip((1.5*generate_data(theta_a, frame)+1.5*generate_data(theta_b, frame)).T,0))  # Update heatmap data]
 
     # snir_b = 20*np.log10(rx_probe(rx_pos_b,theta_b)/rx_probe(rx_pos_b,theta_a))
     # snir_a = 20*np.log10(rx_probe(rx_pos_a,theta_a)/rx_probe(rx_pos_a,theta_b))
-
-
 
     rate_a = np.log2(1+rx_probe(rx_pos_b,theta_b)/(NOISE+rx_probe(rx_pos_b,theta_a)))
     rate_b = np.log2(1+rx_probe(rx_pos_b,theta_b)/(NOISE+rx_probe(rx_pos_b,theta_a)))
@@ -155,25 +159,27 @@ def update(frame):
 
     sum_rate_plot.set_data(angles, sum_rates)
 
+    sum_scatter.set_offsets((angles[-1],sum_rates[-1]))
+
     # print("Probe pos a: "+str(rx_probe(rx_pos_a,theta_b)))
     # print("Probe pos b: "+str(rx_probe(rx_pos_b,theta_b)))
 
 
-    rx_scatter.set_offsets((np.c_[[rx_pos_a[1],rx_pos_b[1]], [rx_pos_a[0],rx_pos_b[0]]]))
+    rx_scatter.set_offsets((np.c_[[rx_pos_a[0],rx_pos_b[0]], [RESOLUTION_V-rx_pos_a[1],RESOLUTION_V-rx_pos_b[1]]]))
 
-    annotationA.set_position((rx_pos_a[1]-8,rx_pos_a[0]+12))
-    annotationA.xy = (rx_pos_a[1],rx_pos_a[0])
+    annotationA.set_position((rx_pos_a[0]-8*4,RESOLUTION_V-rx_pos_a[1]+12*4))
+    annotationA.xy = (rx_pos_a[0],RESOLUTION_V-rx_pos_a[1])
 
     
-    annotationB.set_position((rx_pos_b[1]-8,rx_pos_b[0]+12))
-    annotationB.xy = (rx_pos_b[1],rx_pos_b[0])
+    annotationB.set_position((rx_pos_b[0]-8*4,RESOLUTION_V-rx_pos_b[1]+12*4))
+    annotationB.xy = ((rx_pos_b[0],RESOLUTION_V-rx_pos_b[1]))
 
     return [heatmap, scatter, rx_scatter, annotationA, annotationB, sum_rate_plot]
 
 # Create animation
-ani = animation.FuncAnimation(fig, update, frames=frames, interval=1, blit=False)
+ani = animation.FuncAnimation(fig, update, frames=frames, interval=30, blit=False)
 
 # Save as GIF (optional)
-# ani.save("animated_heatmap_b.gif", writer="pillow")
+# ani.save("animated_heatmap_16ant.gif", writer="pillow",fps=30)
 
 plt.show()
